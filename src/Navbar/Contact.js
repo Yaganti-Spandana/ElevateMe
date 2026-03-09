399b8696258dfc6fc7baf8661e6130bf48635768
YagantiSpandana <yaganti06@gmail.com>
2026-03-09 21:34:06 +0530

    errors rectified

--- src/Navbar/Contact.js
+++ src/Navbar/Contact.js
@@ -18,15 +18,12 @@ const Contact = () => {
     };
 
     try {
-      const res = await fetch("/.netlify/functions/sendEmail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      const data = await res.json();
+      await emailjs.send(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      formData,
+      process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+    );
 
       if (data.ok) {
         alert("Message sent successfully!");
