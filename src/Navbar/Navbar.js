40551cd8e95f2c378bbede97c4fa3a685898f5ae
YagantiSpandana <yaganti06@gmail.com>
2026-03-12 18:22:17 +0530

    modified

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../assets/logo.png";
-import menuIcon from "../assets/logo.png";   // add menu icon image
+import logo from "../images/logo.png";
+import menuIcon from "../images/logo.png";   // add menu icon image
 
 const Navbar = () => {
 
